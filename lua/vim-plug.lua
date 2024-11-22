b556439bfbe497aa1d1bbe590dbdc30557ea699c
arbdigital-art <arb.digitalart@gmail.com>
2024-11-22 16:00:18 -0300

    update

--- lua/vim-plug.lua
+++ lua/vim-plug.lua
@@ -9,10 +9,18 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug('catppuccin/nvim', { as = 'catppuccin' })
 
 Plug 'ap/Vim-css-color'
-Plug 'vim-airline/vim-airline'
 Plug 'navarasu/onedark.nvim'
 Plug 'preservim/nerdtree'
+Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
+Plug 'Xuyuanp/nerdtree-git-plugin' 
+Plug 'sheerun/vim-polyglot'
+-- status bar
+Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
+Plug 'tpope/vim-fugitive'
+Plug 'rbong/vim-flog'
+
+Plug 'honza/vim-snippets'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'ryanoasis/vim-devicons'
